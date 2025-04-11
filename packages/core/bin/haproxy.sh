a7063fc07b949c923e4af6be85cfded3beea29bc
juugis <bruvers.jurgis@gmail.com>
2025-04-11 09:21:10 +0300

    fix haproxy reload

--- packages/core/bin/haproxy.sh
+++ packages/core/bin/haproxy.sh
@@ -6,14 +6,16 @@ echo "--------------------------------------"
 SERVER=${TS_DOMAIN_NAME}
 
 while true; do
-    PID=$(pidof haproxy)
+    # use haproxy graceful reload to avoid downtime
+    # get latest haproxy master process, NOTE: order of pidof output is not guaranteed so this could cause problems
+    PID=$(set -- $(pidof haproxy); echo $1)
 
     if [ -n "$PID" ]; then
         echo "Reloading HAProxy with PID $PID"
-        haproxy -f /usr/local/etc/haproxy/haproxy.cfg -sf $PID | tee -a /var/log/proxy.log
+        haproxy -f /usr/local/etc/haproxy/haproxy.cfg -D -sf $PID >> /var/log/proxy.log 2>&1
     else
         echo "Starting HAProxy (no running process found)"
-        haproxy -f /usr/local/etc/haproxy/haproxy.cfg | tee -a /var/log/proxy.log
+        haproxy -f /usr/local/etc/haproxy/haproxy.cfg -D >> /var/log/proxy.log 2>&1
     fi
 
     sleep $CERTBOT_RESTART_TIME
