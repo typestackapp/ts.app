cbe3e4fd8e43f6b33ecee5ecf940c293c37ab5ee
juugis <bruvers.jurgis@gmail.com>
2025-04-25 08:34:43 +0300

    add assert support

--- packages/wireguard/index.ts
+++ packages/wireguard/index.ts
@@ -1,4 +1,4 @@
-import packagejson from "@ts.app/wireguard/package.json" with { type: "json" }
+import packagejson from "@ts.app/wireguard/package.json" assert { type: "json" }
 import * as env from "@ts.app/wireguard/configs/env.js"
 import { Package, ConfigInput } from "@ts.app/core/common/cli/typestack.js"
 
