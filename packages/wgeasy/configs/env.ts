a738339c86ad6fa3df0d6129a1cd5b5bbced00f0
juugis <bruvers.jurgis@gmail.com>
2025-06-04 13:45:07 +0300

    fix wgeasy proxy

--- packages/wgeasy/configs/env.ts
+++ packages/wgeasy/configs/env.ts
@@ -2,7 +2,8 @@ import { ENV, zod } from "@ts.app/core/common/cli/env.js"
 
 export const wgeasy = new ENV(
     {
-        WGEASY_PORT: zod.coerce.number(),
+        WGEASY_UI_PORT: zod.coerce.number(),
+        WGEASY_WG_PORT: zod.coerce.number(),
         WGEASY_USERNAME: zod.string(),
         WGEASY_PASSWORD: zod.string(),
         WGEASY_HOST: zod.string(),
@@ -10,9 +11,10 @@ export const wgeasy = new ENV(
         WGEASY_IPV6_CIDR: zod.string(),
     },
     {
-        WGEASY_PORT: 51821,
+        WGEASY_UI_PORT: 51821,
+        WGEASY_WG_PORT: 51820,
         WGEASY_USERNAME: "root",
-        WGEASY_PASSWORD: "root-psw",
+        WGEASY_PASSWORD: "root-psw-123",
         WGEASY_HOST: "localhost",
         WGEASY_IPV4_CIDR: " 172.20.0.0/20", // 4096 addresses, 172.20.0.0 to 172.20.15.255
         WGEASY_IPV6_CIDR: "fd00:abcd:1234::/64"
