11ad7211264019f3e6ddd51ab64e1f409d7b1374
juugis <bruvers.jurgis@gmail.com>
2025-06-04 10:41:24 +0300

    fix wgeasy env

--- packages/wgeasy/index.ts
+++ packages/wgeasy/index.ts
@@ -1,5 +1,5 @@
 import packagejson from "@ts.app/wgeasy/package.json" with { type: "json" }
-import * as env from "@ts.app/grafana/configs/env.js"
+import * as env from "@ts.app/wgeasy/configs/env.js"
 import { Package, ConfigInput } from "@ts.app/core/common/cli/typestack.js"
 
 const configs: ConfigInput = { env }
